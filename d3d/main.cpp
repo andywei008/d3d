d3a0b0446b4e6ea87272a8643b05b91bf25bcbd8
andywei008 <160408801@qq.com>
2016-05-06 00:53:08 +0800

    新建D3D工程

---
+++ d3d/main.cpp
@@ -0,0 +1,10 @@
+#include<d3d9.h>
+
+#pragma comment(lib, "d3d9.lib")
+#pragma comment(lib, "d3dx9.lib")
+
+#define WINDOW_CLASS L"UGPDX"             // 宏定义窗口类名称
+#define WINDOW_NAME L"Blank D3D Window"
+
+// 函数声明
+bool InitializeD3D(HWND hWnd, bool  fullscreen);
